bc1ca89fd2b5caf65fc2943646657d2455ab192c
Taremin <taremin.vr@gmail.com>
2025-12-07 01:35:17 +0900

    fix: ComfyUI の init_external_custom_nodes が async に変更されていて、custom_nodes のロードを正しく検知できていなかったのを修正

--- prestartup_script.py
+++ prestartup_script.py
@@ -2,6 +2,17 @@ import nodes
 import os
 import sys
 import importlib
+import inspect
+
+dir = os.path.dirname(__file__)
+me = os.path.basename(dir)
+path = os.path.join(dir, "data.py")
+
+print_orig = print
+
+
+def print(*args):
+    print_orig(*[f"[{me}]:", *args])
 
 
 def load_module(path: str):
@@ -19,8 +30,8 @@ def load_module(path: str):
     return module
 
 
-path = os.path.join(os.path.dirname(__file__), "data.py")
 data = load_module(path)
+
 if hasattr(nodes, "load_custom_nodes"):
     load_custom_nodes = nodes.load_custom_nodes
 elif hasattr(nodes, "init_external_custom_nodes"):
@@ -31,6 +42,7 @@ else:
 
 
 def on_custom_nodes_loaded(function):
+    print(f"register on custom nodes loaded callback: {function}")
     if callable(function):
         data.on_custom_nodes_loaded_callbacks.append(function)
 
@@ -38,6 +50,18 @@ def on_custom_nodes_loaded(function):
 def hooked_load_custom_nodes(*args):
     retval = load_custom_nodes(*args)
 
+    print("on custom nodes loaded")
+    data.NODE_CLASS_MAPPINGS = nodes.NODE_CLASS_MAPPINGS
+    for callback in data.on_custom_nodes_loaded_callbacks:
+        callback(data.NODE_CLASS_MAPPINGS)
+
+    return retval
+
+
+async def hooked_async_init_external_custom_nodes(*args):
+    retval = await load_custom_nodes(*args)
+
+    print("on custom nodes loaded")
     data.NODE_CLASS_MAPPINGS = nodes.NODE_CLASS_MAPPINGS
     for callback in data.on_custom_nodes_loaded_callbacks:
         callback(data.NODE_CLASS_MAPPINGS)
@@ -48,7 +72,10 @@ def hooked_load_custom_nodes(*args):
 if hasattr(nodes, "load_custom_nodes"):
     nodes.load_custom_nodes = hooked_load_custom_nodes
 elif hasattr(nodes, "init_external_custom_nodes"):
-    nodes.init_external_custom_nodes = hooked_load_custom_nodes
+    if inspect.iscoroutinefunction(load_custom_nodes):
+        nodes.init_external_custom_nodes = hooked_async_init_external_custom_nodes
+    else:
+        nodes.init_external_custom_nodes = hooked_load_custom_nodes
 else:
     print("Unsupported ComfyUI version")
     raise AttributeError
